93fd1aff48c49cd34711c48f3727e58430ed6f0d
tanzimtamim007 <tanzimtamim007@gmail.com>
2021-09-30 23:52:14 +0600

    added

---
+++ src/components/Inventory/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h1>This is Inventory</h1>
+            <p>hey there?</p>
+        </div>
+    );
+};
+
+export default Inventory;
